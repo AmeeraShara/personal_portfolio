8e5112df174c3993a41684e9bcbede774596d377
AmeeraShara <ameerashara12@gmail.com>
2025-08-03 18:35:40 +0530

    Initial commit - Personal Portfolio

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Hi, I'm Shara</h1>
+      <p>React Developer | Freelancer</p>
+    </section>
+  );
+}
+
+export default Hero;
